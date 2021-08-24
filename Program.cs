8129558e8c4e19dec26e7a60e370c61a99a6aa1b
Yharen <89000223+Yharen27@users.noreply.github.com>
2021-08-24 19:40:32 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 namespace Text_Based_Rpg
 {
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("You are tasked by the king to vanquished a ferocious dragon.");
+            Console.WriteLine("You are tasked by the king to vanquished a ferocious dragon. -- Press Enter to continue dialog --");
             Console.ReadKey();
             Console.WriteLine("You ventured deep into the dragon's lair.");
             Console.ReadKey();
@@ -13,7 +13,7 @@ namespace Text_Based_Rpg
             Console.ReadKey();
             gameLoop();   
         }
-        static void gameLoop()
+        static void gameLoop() // Loops through until player or dragon's hp is less than or equal to 0
         {
             while (Stats.playerHp > 0 && Stats.dragonHp > 0)
             {
@@ -32,69 +32,66 @@ namespace Text_Based_Rpg
         {
             Console.WriteLine("What do you do?");
             Console.ReadKey();
-            Console.WriteLine("1.Attack\n2.Heal\n3.Defend\n4.Nothing");
-            string playerChoice = Console.ReadLine();
-            if (playerChoice == "Attack")
+            Console.WriteLine("1.Attack\n2.Heal\n3.Defend\n4.Nothing\n5.Quit\nType the word or number");
+            string playerChoice = Convert.ToString(Console.ReadLine()); // Gets input from player
+            if (playerChoice == "Attack" || playerChoice == "1")
             {
                 attackChoice();
             }
-            else if (playerChoice == "Defend")
+            else if (playerChoice == "Heal" || playerChoice == "2")
             {
                 defendChoice();
             }
-            else if (playerChoice == "Heal")
+            else if (playerChoice == "Defend" || playerChoice == "3")
             {
                 healChoice();
             }
-            else if (playerChoice == "Nothing")
+            else if (playerChoice == "Nothing" || playerChoice == "4")
             {
                 nothingChoice();
             }
+            else if (playerChoice == "Quit" || playerChoice == "5")
+            {
+                quitGame();
+            }
         }
+        // Choices
         static void attackChoice()
         {
-            Console.WriteLine("Player has "+Stats.playerHp+"hp and Dragon has "+Stats.dragonHp+"hp.");
-            Console.ReadKey();
             Console.WriteLine("You attacked and dealt "+Stats.playerDmg+"dmg!");
             Stats.dragonHp -= Stats.playerDmg;
+            Console.WriteLine($"Dragon has {Stats.dragonHp} hp left");
             Console.ReadKey();
             dragonAttack();
-            Console.ReadKey();
             gameLoop();
         }
         static void healChoice()
         {
-            Console.WriteLine("Player has "+Stats.playerHp+"hp and Dragon has "+Stats.dragonHp+"hp.");
-            Console.ReadKey();
-            dragonAttack();
-            Console.ReadKey();
             Console.WriteLine("You healed yourself and got "+Stats.playerHeal+"hp.");
             Stats.playerHp += Stats.playerHeal;
+            Console.WriteLine($"Player has {Stats.playerHp} hp left");
             Console.ReadKey();
+            dragonAttack();
             gameLoop();
         }
         static void defendChoice()
         {
-            Console.WriteLine("Player has "+Stats.playerHp+"hp and Dragon has "+Stats.dragonHp+"hp.");
-            Console.ReadKey();
             Console.WriteLine("You blocked the dragon's attack");
             Console.ReadKey();
             gameLoop();
         }
         static void nothingChoice()
         {
-            Console.WriteLine("Player has "+Stats.playerHp+"hp and Dragon has "+Stats.dragonHp+"hp.");
-            Console.ReadKey();
             Console.WriteLine("You did nothing to the dragon's attack.");
             Console.ReadKey();
+            dragonAttack();
             gameLoop();
         }
         static void dragonAttack()
         {
-            Console.WriteLine("Player has "+Stats.playerHp+"hp and Dragon has "+Stats.dragonHp+"hp.");
-            Console.ReadKey();
             Console.WriteLine("The dragon attacked and dealt "+Stats.dragonDmg+"dmg!");
             Stats.playerHp -= Stats.dragonDmg;
+            Console.WriteLine($"Player has {Stats.playerHp} hp left");
             Console.ReadKey();
             gameLoop();
         }
@@ -102,12 +99,16 @@ namespace Text_Based_Rpg
         {
             Console.WriteLine("You've won and vanquished the dragon");
             Console.ReadKey();
-            System.Environment.Exit(0);
+            Main();
         }
         static void playerDefeat()
         {
             Console.WriteLine("You died");
             Console.ReadKey();
+            Main();
+        }
+        static void quitGame()
+        {
             System.Environment.Exit(0);
         }
     }
